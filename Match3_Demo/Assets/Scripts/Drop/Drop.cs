913530ec0969239b142e2dbbf85ee3c763aa9487
Utku Baran <mbaran13@ku.edu.tr>
2022-01-28 16:24:27 +0300

    fix

--- Match3_Demo/Assets/Scripts/Drop/Drop.cs
+++ Match3_Demo/Assets/Scripts/Drop/Drop.cs
@@ -14,6 +14,8 @@ public class Drop : MonoBehaviour, IPooledObject, IDrop
     private Vector3Int positionInfo, previousPositionInfo;
     public Vector3Int PositionInfo { get { return positionInfo; } set { positionInfo = value; } }
 
+    private float destroyTimer = 2f;
+
     private float cellSize;
 
     private void Start()
@@ -31,5 +33,17 @@ public class Drop : MonoBehaviour, IPooledObject, IDrop
     public void OnObjectRespawn()
     {
         return;
+    }
+
+    public void OnMatch()
+    {
+        while (destroyTimer > 0)
+        {
+            destroyTimer -= Time.deltaTime;
+            transform.localScale  = Vector3.Lerp(transform.localScale, Vector3.one * 0.1f, 0.1f);
+        }
+
+        this.gameObject.SetActive(false);
+        transform.position = (Vector3.forward + Vector3.left) * 5f;
     }
 }
